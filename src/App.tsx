cd9139ec3f2c029e06ede7852c277fc83adfdd27
Zeonsi <tomasgarrido0212@gmial.com>
2024-07-24 17:07:32 -0400

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import Home from './pages/Home'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Register from './component/register';
+
 const router = createBrowserRouter([
   {path: '/', element: <Home/>},
-  {path: '/register', element: <Register/>},
   {path: '*', element: <h1>404</h1>},
 ]);
 
