bd2e6997b0ee214980c880c9fc8f08f7dc82c9c7
Zeonsi <tomasgarrido0212@gmial.com>
2024-07-24 11:30:48 -0400

    Page Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Home(){
+    return (
+        <>
+            <header className="container-header">
+                <h1>My Safe Car <span></span></h1>
+            </header>
+        </>
+    );
+}
